401270a5597375973e8ae6a210ccbf7d6a137bff
anurag_kumar <anurag452518@gmail.com>
2026-01-30 14:26:33 +0530

    Mongo db connected

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	
 	"log"
 	"net/http"
+	"url_shortner/db"
 	"url_shortner/routes"
 )
 
 
 func main()  {
+
+	// connect to MongoDB
+	db.ConnectMongoDB()
 	
+	// register all routes
 	routes.RegisterRoutes()
 
 	log.Println("Server started on http://localhost:8080")
